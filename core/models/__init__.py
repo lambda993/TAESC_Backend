24474abf22fd63473adfb1c0d17950f836c45b27
Leopold Bjedov <leopold.bjedov@gmail.com>
2023-06-25 20:04:17 +0200

    Refactored unit model moving out some fields to separate table.

--- core/models/__init__.py
+++ core/models/__init__.py
@@ -6,4 +6,4 @@ from .tedclass import TEDClass
 from .side import UnitSide
 from .sound_category import SoundCategory
 from .weapon import Weapon
-from .unit import Unit
+from .unit import Unit, UnitBasicStats, UnitEconomy, UnitSightOption, UnitWeaponGroup
